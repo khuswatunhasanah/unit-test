fccc253a89ff334a682a986f696fed21d9c2adb0
khuswatunhasanah <91953786+khuswatunhasanah@users.noreply.github.com>
2021-10-27 23:17:05 +0700

    Add files via upload

---
+++ TugasLibraryUnitTest/Day.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class DayTest
+    {
+        [Test]
+        public void NameOfDayTest ()
+        { 
+        
+        }
+    }
+}
